8ec465eb666869e468a3ee14f1e560b004243bd1
Shashwatt000 <sharmashashwat826@gmail.com>
2023-03-25 12:10:17 -0700

    length function

--- len_in_js.js
+++ len_in_js.js
@@ -1,10 +1,8 @@
 const data = require("./users.json")
 // console.log(data);
-data.forEach((element)=>{
-    console.log(element.address.coordinates);
-})
 
-data.forEach((ele)=>{
+
+
   const len = data.length;
-})
+
 console.log(len);
