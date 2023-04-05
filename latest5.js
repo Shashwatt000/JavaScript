46d92ebd844cbf7d50201d522dca4ea33556feaa
Shashwatt000 <sharmashashwat826@gmail.com>
2023-04-05 12:23:17 -0700

    fffff

---
+++ latest5.js
@@ -0,0 +1,9 @@
+let arr = ["vista", "vista", "vista", "alto","alto"];
+let q = arr.filter(checkage)
+function checkage(z) {
+    if (z == "alto") {
+        return z;
+    } ;
+  
+  }
+console.log(q);
