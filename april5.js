11d3384a616db33f51335d1533834c5c73015bf0
Shashwatt000 <sharmashashwat826@gmail.com>
2023-04-05 11:54:29 -0700

    neww

---
+++ april5.js
@@ -0,0 +1,10 @@
+let arr = ["vista", "vista", "vista", "alto"];
+  let z = arr.every(checkAge);
+
+function checkAge(z) {
+    if (z == "vista") {
+        return z;
+    } ;
+  
+  }
+  console.log(z);  
--- aprilfirst.js
+++ aprilfirst.js
@@ -1,3 +1,14 @@
-let arr = ["Vista","Zest","Alto","Nexon"]
-arr.copyWithin(2);
+let arr = ["Vista", "Zest", "Alto", "Nexon"]
+arr.copyWithin(0, 3);
 console.log(arr);
+
+arr.find((name) => {
+    if (name === "Zest") {
+        console.log(true);
+    } else {
+        console.log("false")
+        
+    }
+
+
+})
