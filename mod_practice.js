94f67eff120ded5d5054de691c4e396e6c3b8ad1
Shashwatt000 <sharmashashwat826@gmail.com>
2023-04-24 11:21:35 -0700

    module practice

---
+++ mod_practice.js
@@ -0,0 +1,8 @@
+//console.log(module);
+
+function abc(a,b){
+return  a+b
+}
+ppc = abc(9,8);
+console.log(ppc);
+module.exports = { abc }
--- vinay.js
+++ vinay.js
@@ -1,4 +1,2 @@
 let rollno = [43,88,65,50];
-let vinay = rollno.some(check)
-function check(rollno){return rollno > 55}
-console.log(vinay)
+rollno.pop
