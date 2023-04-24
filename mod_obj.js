01ab3e325fedc8986fecfd4f5d8feac6baae11b0
Shashwatt000 <sharmashashwat826@gmail.com>
2023-04-24 11:42:23 -0700

    module object

---
+++ mod_obj.js
@@ -0,0 +1,7 @@
+
+
+function tommy(fname,lname){
+this.fname = fname
+this.lname = lname
+}
+module.exports = {tommy}
---
+++ mod_obj_2.js
@@ -0,0 +1,3 @@
+const {tommy} = require("./mod_obj");
+var person1 = new tommy('James', 'Bond');
+console.log(person1);
